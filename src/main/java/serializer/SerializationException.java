20f7be73b7a21e147e5ba4fe15bdd6dfe28b24d2
mylonasartist <mylonas_artist@yahoo.com>
2018-12-02 14:44:55 +0200

    SerializationException made public

--- src/main/java/serializer/SerializationException.java
+++ src/main/java/serializer/SerializationException.java
@@ -1,6 +1,6 @@
 package serializer;
 
-class SerializationException extends Exception {
+public class SerializationException extends Exception {
     SerializationException(String message) {
         super(message);
     }
